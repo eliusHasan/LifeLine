155c08a5117412f791fc91c085624463ffea2820
Elius <elius24hasan@gmail.com>
2025-05-11 20:28:06 +0600

    Updated uploadMiddleware to auto-create uploads folder

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,8 +1,15 @@
 const multer = require('multer');
 const path = require("path");
 
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
+
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
+        if (!fs.existsSync('uploads')) {
+        fs.mkdirSync('uploads');
+        }
         cb(null,'uploads/');
     },
     filename: function (req, file, cb) {
